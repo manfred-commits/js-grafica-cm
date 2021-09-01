0cb449589859cb31702a124f8a2564b1fe629abf
manfred-commits <manfredipiraino@gmail.com>
2021-09-01 14:53:34 +0200

    Inizio progetto

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+// Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+// Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+
+
+alert("Hello!");
