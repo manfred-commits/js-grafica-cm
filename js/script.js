b63be8fa0b577bf500a05c3a0d1fd921549f3c71
manfred-commits <manfredipiraino@gmail.com>
2021-09-01 16:48:02 +0200

    aggiunta funzione per generare celle Dinamica

--- js/script.js
+++ js/script.js
@@ -5,9 +5,16 @@
 
 // sezione funzioni
 
-function makePlayground(num){
+
+// 3.Tramite una funzione javascript disegnare in pagina la griglia
+function makePlayground(num,id){
+    for(var i = 0; i < num; i++){
+        document.getElementById(id).innerHTML+=`<div class="square">${i}</div>`;
+    }
 
 }
+
+
 // /sezione funzioni
 
 
@@ -15,14 +22,15 @@ function makePlayground(num){
 
 // sezione codice principale
 
-
-
-
-
-
 // 1.Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
 
 var numeroUtente = parseInt(prompt("Inserisci il numero di celle che devono formare il campo da gioco: "));
+
+// in questa variabile sarà inserito il nome dell'id, del contenitore in cui si vogliono generare le celle
+var id ="game-container";
+
+
+
 
 // 2.Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 
@@ -31,9 +39,9 @@ var numeroUtente = parseInt(prompt("Inserisci il numero di celle che devono form
 // - o impostare, sempre nel css, un container grid che avrà un grid-template-columns pari a repeat(10, 1fr) ed un width di 1000px (cioè 10 celle).
 
 
+makePlayground(numeroUtente, id);
 
-
-
+// 4.
 
 
 
