68fe9bef9293b1a24e5e0c69306fdc4d7e6e1432
manfred-commits <manfredipiraino@gmail.com>
2021-09-01 15:21:02 +0200

    aggiunto test html, base di lavoro js e base css

--- js/script.js
+++ js/script.js
@@ -3,4 +3,38 @@
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 
 
-alert("Hello!");
+// sezione funzioni
+
+function makePlayground(num){
+
+}
+// /sezione funzioni
+
+
+
+
+// sezione codice principale
+
+
+
+
+
+
+// 1.Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+
+var numeroUtente = parseInt(prompt("Inserisci il numero di celle che devono formare il campo da gioco: "));
+
+// 2.Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+
+// Assumendo che le celle siano di dimensione definita, pari a 100px di altezza e 100px di larghezza, possiamo:
+// - o impostare, nel css, un container flex che avrà una larghezza massima pari a 1000px (cioè 10 celle);
+// - o impostare, sempre nel css, un container grid che avrà un grid-template-columns pari a repeat(10, 1fr) ed un width di 1000px (cioè 10 celle).
+
+
+
+
+
+
+
+
+// /sezione codice principale
